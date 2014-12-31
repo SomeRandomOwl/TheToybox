8d65fb3f5905bc6cac4cc6a63f184b18065c5769
Seth177 <ethanary@gmail.com>
2014-12-31 09:52:05 -0600

    updated rock. js added console.logs and error for undefined inputs

--- JavaScript/rock.js
+++ JavaScript/rock.js
@@ -1,48 +1,61 @@
-var dothething = function(){
-var userChoice = prompt("Do you choose rock, paper or scissors?")
-var computerChoice = Math.random()
-
-if (computerChoice >= 0 && computerChoice <= 0.33) {
-    computerChoice = "Rock";
-} else if (computerChoice >= 0.34 && computerChoice <= 0.66) {
-    computerChoice = "Paper";
-} else {
-    computerChoice = "Scissors";
-}
-
-
-
-
-var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
-        return "The result is a tie!";
-    } else if (choice1 === "rock" || choice1 === "Rock") {
-        if (choice2 === "Scissors") {
-            return "You win!";
+var dothething = function() {
+        var userChoice = prompt("Do you choose rock, paper or scissors?")
+        console.log("The Player Chooses" + " " + userChoice)
+        var computerChoice = Math.random()
+        console.log("The Random Number Is:" + " " + computerChoice);
+        if (computerChoice >= 0 && computerChoice <= 0.33) {
+            computerChoice = "Rock";
+        } else if (computerChoice >= 0.34 && computerChoice <= 0.66) {
+            computerChoice = "Paper";
         } else {
-            return "The Computer wins!";
+            computerChoice = "Scissors";
         }
-    } else if (choice1 === "paper" || choice1 === "Paper") {
-        if (choice2 === "Rock") {
-            return "You win!";
-        } else {
-            return "The Computer wins!";
+
+        console.log("The Computer Chooses:" + " " + computerChoice);
+
+
+        var compare = function(choice1, choice2) {
+            if (choice1 === choice2) {
+                return "The result is a tie!";
+            } else if (choice1 === "rock" || choice1 === "Rock") {
+                if (choice2 === "Scissors") {
+                    return "You win!";
+                    console.log('You Win!');
+                } else {
+                    return "The Computer wins!";
+                    console.log("The Computer Wins!");
+                }
+            } else if (choice1 === "paper" || choice1 === "Paper") {
+                if (choice2 === "Rock") {
+                    return "You win!";
+                    console.log('You Win!');
+                } else {
+                    return "The Computer wins!";
+                    console.log("The Computer Wins!");
+
+                }
+            } else if (choice1 === "scissors" || choice1 === "Scissors") {
+                if (choice2 === "Paper") {
+                    return "You win!";
+                    console.log('You Win!');
+                } else {
+                    return "The Computer wins!";
+                    console.log("The Computer Wins!");
+
+                }
+            } else {
+                return "Error! Unrecgonized Choice!";
+                console.log('Error, undefined Choice');
+            }
         }
-    } else if (choice1 === "scissors" || choice1 === "Scissors") {
-        if (choice2 === "Paper") {
-            return "You win!";
+
+        var inform = confirm('The Computer Chooses:' + " " + computerChoice)
+        if (inform === true) {
+            var results = compare(userChoice, computerChoice);
+            alert(results)
         } else {
-            return "The Computer wins!";
+            console.log('error');
         }
     }
-}
-var inform = confirm('The Computer Chooses:' + " " + computerChoice)
-if (inform === true) {
-    var results = compare(userChoice, computerChoice);
-    alert(results)
-} else {
-    console.log('error');
-}
-}
-//This does something
+    //This does something
 dothething()
