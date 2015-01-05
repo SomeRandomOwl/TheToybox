be86f38a50d87471f90c59f901f4a3fd5025bd6f
Ethan Ary <ethanary@gmail.com>
2015-01-05 12:02:30 -0600

    Made Scratchpad.js look nicer

--- JavaScript/scratchpad.js
+++ JavaScript/scratchpad.js
@@ -2,35 +2,27 @@ console.log('Scratchpad Script is now running!');
 
 var code = prompt("Continue with the code stuff?")
 
-    if(code.toLowerCase() === "yes")
-        {  
-             console.log('Yey Script time!');
+    if(code.toLowerCase() === "yes"){  
+        console.log('Yey Script time!');
 
-             var yey = prompt("Yey?")
-                if(yey.toLowerCase() === "yey")
-                    {
+            var yey = prompt("Yey?")
+                if(yey.toLowerCase() === "yey"){
                          alert("This makes me happy!");
-                    }
-                else
-                    {
+                } else if(yey.toLowerCase() === "doctor?"){
+                    
+                } else {
                         alert("You make me sad now....");
-                    }
+                }
          }
 
 
-    else if(code.toLowerCase() === "no")
-        {
+    else if(code.toLowerCase() === "no"){
             console.log('Really I cant show you my coding things?');
 
             alert("aaw...no code stuff");
-        }
-    else if(code === null)
-        {
+        } else if(code === null){
             console.log("User cancled the diolog Box");
-        }
-    else
-        {
+        } else {
             console.log('Error no recgonized input!');
-            
             alert("Error, Invalid input");
         }
