66a98c4f25ac528f8b1af43a120f591c5c08ae77
Ethan Ary <ethanary@gmail.com>
2015-01-14 12:59:51 -0600

    Made the replayer nicer

--- JavaScript/rock.js
+++ JavaScript/rock.js
@@ -3,88 +3,83 @@ var dothething = ""
 var userChoice = ""
 var computerChoice = ""
 var done = false
-//----------------
+    //----------------
 var doTheThing = function() {
-        var userChoice = prompt("Do you choose rock, paper or scissors?")
-        console.log("The Player Chooses" + " " + userChoice)
-        var computerChoice = Math.random()
-        console.log("The Random Number Is:" + " " + computerChoice);
-        if (computerChoice >= 0 && computerChoice <= 0.33) {
-            computerChoice = "Rock";
-        } else if (computerChoice >= 0.34 && computerChoice <= 0.66) {
-            computerChoice = "Paper";
-        } else {
-            computerChoice = "Scissors";
-        }
-        console.log("The Computer Chooses:" + " " + computerChoice);
-        var compare = function(choice1, choice2) {
-            if (choice1.toLowerCase() === choice2.toLowerCase()) {
-                return "The result is a tie!";
-                console.log('It is a Tie!');
-                var done = true
-            } else if (choice1.toLowerCase() === "rock") {
-                if (choice2 === "Scissors") {
-                    return "You win!";
-                    console.log('You Win!');
-                    var done = true
-                } else {
-                    return "The Computer wins!";
-                    console.log("The Computer Wins!");
-                    var done = true
-                }
-            } else if (choice1.toLowerCase() === "paper") {
-                if (choice2 === "Rock") {
-                    return "You win!";
-                    console.log('You Win!');
-                    var done = true
-                } else {
-                    return "The Computer wins!";
-                    console.log("The Computer Wins!");
-                    var done = true
-                }
-            } else if (choice1.toLowerCase() === "scissors") {
-                if (choice2 === "Paper") {
-                    return "You win!";
-                    console.log('You Win!');
-                    var done = true
-                } else {
-                    return "The Computer wins!";
-                    console.log("The Computer Wins!");
-                    var done = true
-                }
+        do {
+            var userChoice = prompt("Do you choose rock, paper or scissors?")
+            console.log("The Player Chooses" + " " + userChoice)
+            var computerChoice = Math.random()
+            console.log("The Random Number Is:" + " " + computerChoice);
+            if (computerChoice >= 0 && computerChoice <= 0.33) {
+                computerChoice = "Rock";
+            } else if (computerChoice >= 0.34 && computerChoice <= 0.66) {
+                computerChoice = "Paper";
             } else {
-                return "Error! Unrecgonized Choice!";
-                console.log('Error, undefined Choice');
-                var done = false
+                computerChoice = "Scissors";
             }
-        }
-        var inform = confirm('The Computer Chooses:' + " " + computerChoice)
-        if (inform === true) {
-            var results = compare(userChoice, computerChoice);
-            alert(results)
-        } else {
-            console.log('error');
-        }
-        return true
+            console.log("The Computer Chooses:" + " " + computerChoice);
+            var compare = function(choice1, choice2) {
+                if (choice1.toLowerCase() === choice2.toLowerCase()) {
+                    return "The result is a tie!";
+                    console.log('It is a Tie!');
+                    var done = true
+                } else if (choice1.toLowerCase() === "rock") {
+                    if (choice2 === "Scissors") {
+                        return "You win!";
+                        console.log('You Win!');
+                        var done = true
+                    } else {
+                        return "The Computer wins!";
+                        console.log("The Computer Wins!");
+                        var done = true
+                    }
+                } else if (choice1.toLowerCase() === "paper") {
+                    if (choice2 === "Rock") {
+                        return "You win!";
+                        console.log('You Win!');
+                        var done = true
+                    } else {
+                        return "The Computer wins!";
+                        console.log("The Computer Wins!");
+                        var done = true
+                    }
+                } else if (choice1.toLowerCase() === "scissors") {
+                    if (choice2 === "Paper") {
+                        return "You win!";
+                        console.log('You Win!');
+                        var done = true
+                    } else {
+                        return "The Computer wins!";
+                        console.log("The Computer Wins!");
+                        var done = true
+                    }
+                } else {
+                    return "Error! Unrecgonized Choice!";
+                    console.log('Error, undefined Choice');
+                    var done = false
+                }
+            }
+            var inform = confirm('The Computer Chooses:' + " " + computerChoice)
+            if (inform === true) {
+                var results = compare(userChoice, computerChoice);
+                alert(results)
+            } else {
+                console.log('error');
+            }
+            if (!confirm("go again?")) {
+                return;
+            }
+
+        } while (true)
     }
-//This does something 
+    //This does something 
 var meep = function() {
-    var rock = prompt("Want to play a game")
-    if (rock.toLowerCase() === "yes") {
-        dothething = doTheThing()
-    } else {
-        console.log("meh… ")
+        var rock = prompt("Want to play a game")
+        if (rock.toLowerCase() === "yes") {
+            dothething = doTheThing()
+        } else {
+            console.log("meh… ")
+        }
     }
-}
-//Starts the game
+    //Starts the game
 meep()
-//Checks for game to be compleated and asks to replay the game
-if (dothething === true) {
-    var play = prompt("Play again?")
-    if (play.toLowerCase() === "yes") {
-        dothething = false
-        dotgething = doTheThing()
-    } else {
-        confirm("Ok have a nice day")
-    }
-}
