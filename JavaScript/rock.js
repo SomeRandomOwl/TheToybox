ea39a33698ad81bbff8a98057e134669469238ee
Ethan Ary <ethanary@gmail.com>
2015-01-14 13:59:35 -0600

    Reworded the play again confirmation box

--- JavaScript/rock.js
+++ JavaScript/rock.js
@@ -66,7 +66,7 @@ var doTheThing = function() {
             } else {
                 console.log('error');
             }
-            if (!confirm("go again?")) {
+            if (!confirm("Play Again?")) {
                 return;
             }
 
