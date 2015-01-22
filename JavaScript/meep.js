46a14f69077b140ad0d156e2cbf987ee282fc5b5
Ethan Ary <ethanary@gmail.com>
2015-01-22 12:29:39 -0600

    Added meep.js, changed the style into main.css

--- JavaScript/meep.js
+++ JavaScript/meep.js
@@ -1,16 +1,22 @@
-var meep = confirm("Meep?");
-
-switch(meep) {
-    case(true):
-        console.log("meep meep");
-        break;
-    case(false):
-        console.log("No meep....");
-        break;
-    case("Meep"):
-        console.log("meep?....");
-        break;
+var user = prompt("Do you like to meep?").toLowerCase();
+switch (user) {
+    case 'yes':
+        console.log("Good...good...");
+    break;
+    case 'no':
+        console.log("NON MEEPER!");
+    break;
+    case 'meep':
+        console.log("Excellent, you can continue!");
+        var who = prompt("Who are you?","Someone?").toLowerCase();
+            if (user === "meep" && who === "seth177") {
+                console.log("Welcome Master of meeping");
+            } else if (who === "meep" || who === "meep meep") {
+                console.log("Welcome " + who + " This is a script made by the Master meeper");
+            }
+            
+    break;
     default:
-        console.log("This isnt even possible...what did you do?");
-        break;
+        console.log("Well do you like to meep or not");
+    break;
 }
