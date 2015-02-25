4755ca0dfb999dc2b9f633aeccaa5d5f8746f85c
RasPi Info <root@RaspPi>
2015-02-25 16:44:16 -0600

    save

--- index.php
+++ index.php
@@ -24,81 +24,67 @@
 # Copyright (c) 2002, 2011 Jochen Kupperschmidt <http://homework.nwsnet.de/>
 # Released under the terms of the MIT license
 # <http://www.opensource.org/licenses/mit-license.php>
-
-
 ### configuration
-
 # Show the local path. Disable this for security reasons.
 define('SHOW_PATH', FALSE);
-
 # Show a link to the parent directory ('..').
 define('SHOW_PARENT_LINK', FALSE);
-
 # Show "hidden" directories and files, i.e. those whose names
 # start with a dot.
 define('SHOW_HIDDEN_ENTRIES', FALSE);
-
 ### /configuration
-
-
 function get_grouped_entries($path) {
-    list($dirs, $files) = collect_directories_and_files($path);
-    $dirs = filter_directories($dirs);
-    $files = filter_files($files);
-    return array_merge(
-        array_fill_keys($dirs, TRUE),
-        array_fill_keys($files, FALSE));
+list($dirs, $files) = collect_directories_and_files($path);
+$dirs = filter_directories($dirs);
+$files = filter_files($files);
+return array_merge(
+array_fill_keys($dirs, TRUE),
+array_fill_keys($files, FALSE));
 }
-
 function collect_directories_and_files($path) {
-    # Retrieve directories and files inside the given path.
-    # Also, `scandir()` already sorts the directory entries.
-    $entries = scandir($path);
-    return array_partition($entries, function($entry) {
-        return is_dir($entry);
-    });
+# Retrieve directories and files inside the given path.
+# Also, `scandir()` already sorts the directory entries.
+$entries = scandir($path);
+return array_partition($entries, function($entry) {
+return is_dir($entry);
+});
 }
-
 function array_partition($array, $predicate_callback) {
-    # Partition elements of an array into two arrays according
-    # to the boolean result from evaluating the predicate.
-    $results = array_fill_keys(array(1, 0), array());
-    foreach ($array as $element) {
-        array_push(
-            $results[(int) $predicate_callback($element)],
-            $element);
-    }
-    return array($results[1], $results[0]);
+# Partition elements of an array into two arrays according
+# to the boolean result from evaluating the predicate.
+$results = array_fill_keys(array(1, 0), array());
+foreach ($array as $element) {
+array_push(
+$results[(int) $predicate_callback($element)],
+$element);
 }
-
+return array($results[1], $results[0]);
+}
 function filter_directories($dirs) {
-    # Exclude directories. Adjust as necessary.
-    return array_filter($dirs, function($dir) {
-        return $dir != '.'  # current directory
-            && (SHOW_PARENT_LINK || $dir != '..') # parent directory
-            && !is_hidden($dir);
-    });
+# Exclude directories. Adjust as necessary.
+return array_filter($dirs, function($dir) {
+return $dir != '.'  # current directory
+&& (SHOW_PARENT_LINK || $dir != '..') # parent directory
+&& !is_hidden($dir);
+});
 }
-
 function filter_files($files) {
-    return array_filter($files, function($file) {
-        return !is_hidden($file)
-            && substr($file, -4) != '.php' # PHP scripts
-            && substr($file, -4) != '.settings' #To hide sensitive data
-            && substr($file, -4) != '.ftpquota' #To hide ftp Things
-            && substr($file, -4) != '*.html'
-            && substr($file, -4) != '*.sublime-project'
-            && substr($file, -4) != '*.sublime-workspace';
-    });
+return array_filter($files, function($file) {
+return !is_hidden($file)
+&& substr($file, -4) != '.php' # PHP scripts
+&& substr($file, -4) != '.settings' #To hide sensitive data
+&& substr($file, -4) != '.ftpquota' #To hide ftp Things
+&& substr($file, -4) != '*.html'
+&& substr($file, -4) != '*.sublime-project'
+&& substr($file, -4) != '*.sublime-workspace';
+});
 }
-
 function is_hidden($entry) {
-    return !SHOW_HIDDEN_ENTRIES
-        && substr($entry, 0, 1) == '.'  # Name starts with a dot.
-        && $entry != '.'  # Ignore current directory.
-        && $entry != '..';  # Ignore parent directory.
+return !SHOW_HIDDEN_ENTRIES
+&& substr($entry, 0, 1) == '.'  # Name starts with a dot.
+&& $entry != '.'  # Ignore current directory.
+&& $entry != '..';  # Ignore parent directory.
 }
-
 $path = __DIR__ . '/';
 $entries = get_grouped_entries($path);
 ?>
@@ -114,110 +100,95 @@ $entries = get_grouped_entries($path);
     <link rel="stylesheet" type="text/css" href="/assets/main.css">
     <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="/assets/cover.css" rel="stylesheet">
-      <script src="/secret/TheToybox/JavaScript/Games/rock.js"></script>
+    <script src="/secret/TheToybox/JavaScript/Games/rock.js"></script>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-
   <body>
-
     <style type="text/css">
-  li.directory a:before {
-  content: '[ ';
-  }
-  li.directory a:after {
-  content: ' ]';
-  }
-  </style>
-
-
+    li.directory a:before {
+    content: '[ ';
+    }
+    li.directory a:after {
+    content: ' ]';
+    }
+    </style>
     <div class="site-wrapper">
-
       <div class="site-wrapper-inner">
-
         <div class="cover-container">
-
-
-          <div>
-            <div class="inner">
-              <h3 class="masthead-brand">Seth177's Random place</h3>
-              <nav>
-                <ul class="nav masthead-nav">
-                  <li><a href="/">Home</a></li>
-                  <li class="active"><a href="/secret/TheToybox">Scripts</a></li>
-                  <li><a href="/contact">Contact</a></li>
-                </ul>
-              </nav>
+          <div id="color">
+            <div class="masthead clearfix" id="head">
+              <div class="inner">
+                <h3 class="masthead-brand">Seth177's Random place</h3>
+                <nav>
+                  <ul class="nav masthead-nav">
+                    <li><a href="/">Home</a></li>
+                    <li class="active"><a href="/secret/TheToybox">Scripts</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                  </ul>
+                </nav>
+              </div>
+              <hr color="#004466">
             </div>
-            <hr color="#004466">
           </div>
-
-          <div class="inner cover" id="body">
-
-      <h1><a href="/secret/TheToybox">The Toybox</a></h1>
-      <h2>Current active script is rock.js</h2>
-      <h3><a type="submit" value="Start Script" class="btn btn-lg btn-default">Start Script</a>
-      <hr>
-      </div>
-</div>
-<div class="scrollbar-macosx" width="100%" height="100%">
-    <article>
-      <h1 align="Center">Content of <?php echo SHOW_PATH ? '<em>' . $path . '</em>' : 'this directory'; ?></h1>
-      <h1 align="center"><p> This is used for Script testing From here you can see all the scripts I've messed with</p></h1>
-      <h5 align="center">Alot of my scripts require the console log</h5>
-      <h1>
-      <ul>
-  <div align="left">
-<?php
-foreach ($entries as $entry => $is_dir) {
-    $class_name = $is_dir ? 'directory' : 'file';
-    $escaped_entry = htmlspecialchars($entry);
-    printf('        <li class="%s"><a href="%s">%s</a></li>' . "\n",
-        $class_name, $escaped_entry, $escaped_entry);
-}
-?>
-      </ul>
-    </article>
+        </div>
+          <div align="center">
+              <h1>Current Active Script: Rock.js</h1>
+              <a type="submit" value="Start Script" class="btn btn-lg btn-default">Start Script</a>
           </div>
-
+        <div class="scrollbar-macosx" width="100%" height="100%">
+          <article>
+          <hr>
+            <h1 align="Center">Content of <?php echo SHOW_PATH ? '<em>' . $path . '</em>' : 'this directory'; ?></h1>
+            <h1 align="center"><p> This is used for Script testing From here you can see all the scripts I've messed with</p></h1>
+            <h5 align="center">Alot of my scripts require the console log</h5>
+            <h1>
+            <ul>
+              <div align="left">
+                <?php
+                foreach ($entries as $entry => $is_dir) {
+                $class_name = $is_dir ? 'directory' : 'file';
+                $escaped_entry = htmlspecialchars($entry);
+                printf('        <li class="%s"><a href="%s">%s</a></li>' . "\n",
+                $class_name, $escaped_entry, $escaped_entry);
+                }
+                ?>
+              </ul>
+            </article>
+          </div>
           <div>
             <div>
+              <hr>
               <p>Made by <a href="http://steamcommunity.com/id/Seth177/" id="steam">seth177</a></p>
             </div>
           </div>
-
         </div>
-
       </div>
-
     </div>
-
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-             <script>
-      $(function() {
-      $( "a[type=submit]" )
-      .button()
-      .click(function( event ) {
-      start();
-      });
-      });
-      </script>
+    <script>
+    $(function() {
+    $( "a[type=submit]" )
+    .button()
+    .click(function( event ) {
+    start();
+    });
+    });
+    </script>
     <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
     <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-47261149-3', 'auto');
-  ga('send', 'pageview');
-
-</script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-47261149-3', 'auto');
+    ga('send', 'pageview');
+    </script>
   </body>
 </html>
