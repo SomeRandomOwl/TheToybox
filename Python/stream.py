9f1e852981d5cd7ffe2eb893284f7bc629c88005
Ethan Ary <ethanary@gmail.com>
2015-03-23 21:56:11 -0500

    Added stream.py Which is used to start streams in vlc media player a little bit easier

---
+++ Python/stream.py
@@ -0,0 +1,12 @@
+# Importent notes this uses the program http://docs.livestreamer.io/index.html which provides the livestreamer command
+# This python program is used to open twitch streams in vlc media player
+
+import os
+service = ''
+stream = ''
+streaming = ''
+service = input('What stream service?: ')
+stream = input('What stream?:')
+streaming = 'livestreamer twitch.tv/' + stream + ' source'
+os.system(streaming)
+input("Press Enter to continue...")
